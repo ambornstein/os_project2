aa3082cabbf1b407b44e70860073e1111f169e44
Oliver Thode <omthode@wpi.edu>
2022-02-01 23:16:44 -0500

    Checkpoint commit
    setup done for both parts

--- spectacular.c
+++ spectacular.c
@@ -6,46 +6,60 @@
 #include <sys/wait.h>
 #include <semaphore.h>
 
-#define NUM_JUGGLERS 11; //type 0
-#define NUM_DANCERS 17; //type 1
-#define NUM_SOLO 6; //type 2
+#define NUM_JUGGLERS 11 //type 0
+#define NUM_DANCERS 17 //type 1
+#define NUM_SOLO 6 //type 2
 
+// semaphores
 sem_t mutex;
-sem_t slots; //empty: 2 for soloists, otherwise 4 (should start at 4)
 sem_t jug_lock;
 sem_t dance_lock;
 sem_t solo_lock;
-int type = -1;
+
+// globals
+int active_type = -1;
+int slots; //empty: 2 for soloists, otherwise 4 (should start at 4)
 pthread_t stage_space[4];
 
-void join_stage(int input) {
+typedef struct {
+    int type;
+    int number;
+    int ready_time;
+} performer_t;
+
+void * join_stage(void *input) {
     // cast and dereference input argument
-    printf("I am %s number %d\n", ((input == 0) ? "juggler" : (input == 1) ? "dancer" : "soloist"), 1);
+    performer_t *info = (performer_t *) input;
+
+    // getting ready
+    sleep(info->ready_time);
+
+    printf("I am %s #%d\n", ((info->type == 0) ? "Juggler" : (info->type == 1) ? "Dancer" : "Soloist"), info->number);
     sem_wait(&mutex);//Start crit section
-    if (type == -1) type = input;
-    switch(input) {
-        case 0: sem_post(&jug_lock); break;
-        case 1: sem_post(&dance_lock); break;
-        case 2: sem_post(&solo_lock); break;
+    if (active_type == -1) {
+        active_type = info->type;
+        switch(info->type) {
+            case 0: sem_post(&jug_lock); break;
+            case 1: sem_post(&dance_lock); break;
+            case 2: sem_post(&solo_lock); break;
+        }
     }
     int r = 0;
-    int y;
-    sem_getvalue(&slots, &y);
-    printf("%d",y);
-    if (y != 0) {
-        while (r < y) {
+    if (slots != 0) {
+        while (r < slots) {
             if (stage_space[r] == NULL) {
                 stage_space[r] = pthread_self();
-                sem_wait(&slots);
                 break;
             }
+            else r++;
         }
     }
     else {
-        printf("On stage:");
+        printf("On stage: ");
         for (int n = 0; n < 4; n++) {
             printf("%ld",stage_space[n]);
         }
+        printf("\n");
     }
     sem_post(&mutex);//End crit section
 }
@@ -57,7 +71,7 @@ void join_stage(int input) {
     sem_post(&mutex);//End crit section
 }*/
 
-void *juggler() {
+/*void *juggler() {
     join_stage(0);
     sem_wait(&jug_lock);
     sleep(1);
@@ -79,27 +93,43 @@ void *soloist() {
     sleep(1);
     printf("Soloed");
     sem_post(&solo_lock);
-}
+}*/
 
 void performer_init() {
-    sem_init(&slots, 0, 4);
+    int num_performers = NUM_JUGGLERS + NUM_DANCERS + NUM_SOLO;
+    pthread_t tid[num_performers];
+
+    performer_t *performers = (performer_t *) malloc(num_performers * sizeof(performer_t));
+
     sem_init(&jug_lock, 0, 0);
     sem_init(&dance_lock, 0, 0);
     sem_init(&solo_lock, 0, 0);
-    for (int i = 0; i < 11; i++) {
-        pthread_t jug;
-        pthread_create(&jug, NULL, juggler, NULL);
-        pthread_join(jug, NULL);
+    for (int i = 0; i < NUM_JUGGLERS; i++) {
+        performer_t* p = performers + i;
+        p->type = 0;
+        p->number = i;
     }
-    for (int j = 0; j < 17; j++) {
-        pthread_t danc;
-        pthread_create(&danc, NULL, dancer, NULL);
-        pthread_join(danc, NULL);
+    for (int j = 0; j < NUM_DANCERS; j++) {
+        performer_t* p = performers + NUM_JUGGLERS + j;
+        p->type = 1;
+        p->number = j;
     }
-    for (int k = 0; k < 6; k++) {
-        pthread_t solo;
-        pthread_create(&solo, NULL, soloist, NULL);
-        pthread_join(solo, NULL);
+    for (int k = 0; k < NUM_SOLO; k++) {
+        performer_t* p = performers + NUM_JUGGLERS + NUM_DANCERS + k;
+        p->type = 2;
+        p->number = k;
+    }
+
+    // creating threads
+    for (int thread = 0; thread < num_performers; thread++) {
+        performer_t* p = performers + thread;
+        p->ready_time = (rand() % 5) + 1;
+        pthread_create(&tid[thread], NULL, &join_stage, p);
+    }
+
+    // joining threads
+    for (int thread = 0; thread < num_performers; thread++) {
+        pthread_join(tid[thread], NULL);
     }
 }
 
